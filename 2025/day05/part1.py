2948b41e47294f23f4a69995332910abb1fa5931
Jorge L. Arienza <sr_gee@yahoo.com>
2025-12-20 13:35:52 +0100

    feat: add working solution for part1

--- 2025/day05/part1.py
+++ 2025/day05/part1.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 def get_input(filename: str) -> str:
     '''Read input file and returns a list of text clean lines (removes end of line characters).'''
-    
     file_path = Path(__file__).parent / filename
     with file_path.open() as f:
         input_lines = []
@@ -13,11 +12,28 @@ def get_input(filename: str) -> str:
     return input_lines
 
 
-def solve(data):
-    # TODO: implment solution
-    pass
-    
+def solve(data: list[str]) -> int:
+    sep_index = data.index('')
 
+    # Parse fresh ranges. NOTE: python ranges are not both-inclusive so we need to add 1 to upper bound
+    fresh_ranges = []
+    for i in range(sep_index):
+        range_limits = data[i].split('-')
+        fresh_ranges.append(range(int(range_limits[0]), int(range_limits[1])+1))
+    #print(f'Parsing fresh ranges: {fresh_ranges}')
+
+    # Parse available ingredients
+    ingredients = [int(data[i]) for i in range(sep_index+1, len(data))]
+    #print(f'Available ingredientes: {ingredients}')
+
+    # Count available fresh ingredients
+    count = 0
+    for id in ingredients:
+        if any(id in r for r in fresh_ranges):
+            count += 1
+
+    return count
+    
 if __name__ == '__main__':
     print(f'Sample: {solve(get_input('sample.txt'))}')
     print(f'Solution: {solve(get_input('input.txt'))}')
