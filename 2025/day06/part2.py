54d8165469783b3d8f9ce702ea3bd5373348f5f3
Jorge L. Arienza <sr_gee@yahoo.com>
2025-12-20 20:47:16 +0100

    feat: add working solution for part2

--- 2025/day06/part2.py
+++ 2025/day06/part2.py
@@ -1,22 +1,59 @@
-
 from pathlib import Path
+import math
 
-def get_input(filename: str) -> str:
-    '''Read input file and returns a list of text clean lines (removes end of line characters).'''
-    
+
+def get_input(filename: str) -> str:    
     file_path = Path(__file__).parent / filename
     with file_path.open() as f:
-        input_lines = []
-        while line := f.readline():
-            input_lines.append(line.strip())
+        return f.readlines()
+
+def solve(data: list[str]) -> int:
+    # Parse operands into a list of list. The inner list are the operands per problem and the outer list contains the problems.
+    num_cols = len(data[0]) # Max number of columns including \n
+    num_rows = len(data)
+    operands, problems = [], []
+    for i in range(num_cols-1):
+        number = [data[n][-2-i] for n in range(num_rows-1) if data[n][-2-i] != ' ']
+        if not number:
+            problems.append(operands)
+            operands = []
+            continue
+        operands.append(int(''.join(number)))
+    else:
+        problems.append(operands)
+    #print(problems)
+
+    # Parse operators
+    operators = data[-1].split()
+    #print(operators)
+
+    # Do the math
+    totals = []
+    for operation, numbers in zip(reversed(operators), problems):
+        #print(operation, numbers)
+        if operation == '+':
+            totals.append(sum(numbers))
+        elif operation == '*':
+            totals.append(math.prod(numbers))
     
-    return input_lines
+    return sum(totals)
 
 
-def solve(data):
-    # TODO: implement solution
-    pass
-    
+    """     accumulated = 0
+        step = num_rows - 1
+        #print(step)
+        start, stop = 0, step
+        for i in range(len(operators)):
+            #print(start, stop)
+            if operators[-1-i] == '*':
+                result = math.prod(operands[start:stop])
+            elif operators[-1-i] == '+':
+                result = sum(operands[start:stop])
+            else:
+                result = 0
+            accumulated += result
+            start, stop = stop, stop + step
+    """
 
 if __name__ == '__main__':
     print(f'Sample: {solve(get_input('sample.txt'))}')
