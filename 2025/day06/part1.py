9b1177f3ccf76434e0983441320226b564d88986
Jorge L. Arienza <sr_gee@yahoo.com>
2025-12-20 16:40:50 +0100

    feat: add working solution for part1

--- 2025/day06/part1.py
+++ 2025/day06/part1.py
@@ -1,5 +1,5 @@
-
 from pathlib import Path
+import math
 
 def get_input(filename: str) -> str:
     '''Read input file and returns a list of text clean lines (removes end of line characters).'''
@@ -8,15 +8,26 @@ def get_input(filename: str) -> str:
     with file_path.open() as f:
         input_lines = []
         while line := f.readline():
-            input_lines.append(line.strip())
+            input_lines.append(line.split())
     
     return input_lines
 
-
-def solve(data):
-    # TODO: implment solution
-    pass
+def solve(data: list[str]) -> int:
+    max_row, max_col = len(data), len(data[0])
+    accumulated = 0
+    for col in range(max_col):
+        *operands, operator = [int(data[i][col]) if i < max_row-1 else data[i][col] for i in range(max_row)]
+        #print(f'Column {col}: {operands} | Operator: {operator}')
+        if operator == '*':
+            result = math.prod(operands)
+        elif operator == '+':
+            result = sum(operands)
+        else:
+            result = 0
+        accumulated += result
     
+    return accumulated
+
 
 if __name__ == '__main__':
     print(f'Sample: {solve(get_input('sample.txt'))}')
