ed61fa33ba682c764c2e258438143c9f6fbf38d5
Jorge L. Arienza <sr_gee@yahoo.com>
2025-12-21 17:58:56 +0100

    feat: add working solution for part2

--- 2025/day07/part2.py
+++ 2025/day07/part2.py
@@ -1,22 +1,45 @@
+from part1 import get_input
+from collections import namedtuple
+from functools import cache
 
-from pathlib import Path
 
-def get_input(filename: str) -> str:
-    '''Read input file and returns a list of text clean lines (removes end of line characters).'''
-    
-    file_path = Path(__file__).parent / filename
-    with file_path.open() as f:
-        input_lines = []
-        while line := f.readline():
-            input_lines.append(line.strip())
-    
-    return input_lines
 
 
 def solve(data):
-    # TODO: implement solution
-    pass
+    '''
+    Uses a recursive DFS algorithm with memoization.
     
+    Since the beams always move down, there are no loops. The recursive function answers this: 
+    “If I am at this point, how many completed timelines will result?”
+
+    Base Case: If I’m off the grid, I’ve finished one valid timeline. Return 1.
+    Recursive Step:
+        If I’m at a ^: result is count_timelines(left) + count_timelines(right).
+        Otherwise: result is count_timelines(down).
+
+    '''
+    Point = namedtuple('Point', 'row col')
+    start = Point(0, data[0].find('S'))
+    max_row, max_col = len(data) - 1, len(data[0]) - 1
+
+    @cache
+    def count_timelines(point:Point) -> int:
+        '''Recursive DFS to count timelines that result from a particle at "point".'''
+        if point.row > max_row or point.col > max_col:
+            # The particle has exited the manifold. It counts as a valid timeline.
+            return 1
+        
+        if data[point.row][point.col] == '^':
+            # Split. Both new timelines continue downward.
+            left = Point(point.row+1, point.col-1)
+            right = Point(point.row+1, point.col+1)
+            return count_timelines(left) + count_timelines(right)
+        else:
+            # Continue downward
+            return count_timelines(Point(point.row+1, point.col))
+
+    return count_timelines(start)
+  
 
 if __name__ == '__main__':
     print(f'Sample: {solve(get_input('sample.txt'))}')
