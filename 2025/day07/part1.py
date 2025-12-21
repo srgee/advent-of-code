1cb401d6207f74a79655c508c24d9b742a8e56a3
Jorge L. Arienza <sr_gee@yahoo.com>
2025-12-21 17:28:39 +0100

    feat: add working solution for part1

--- 2025/day07/part1.py
+++ 2025/day07/part1.py
@@ -1,6 +1,8 @@
 
 from pathlib import Path
 
+from collections import deque, namedtuple
+
 def get_input(filename: str) -> str:
     '''Read input file and returns a list of text clean lines (removes end of line characters).'''
     
@@ -13,10 +15,43 @@ def get_input(filename: str) -> str:
     return input_lines
 
 
-def solve(data):
-    # TODO: implment solution
-    pass
+def solve(data: list[str]) -> int:
+    '''Implement BFS algorithm usinf a doble-ended queue and a set for explored cells. Whenever we hit a splitter we increment count and and split the beam.'''
+    Point = namedtuple('Point', 'row col')
+
+    # Parse input data into a list of splitter coordinates (row, col)
+    splitter_coords = []
+    max_row, max_col = len(data), len(data[0])
+    for i in range(max_row):
+        for j in range(max_col):
+            if data[i][j] == '^':
+                splitter_coords.append(Point(i, j))
     
+    start = Point(0, data[0].find('S'))
+    splits = 0
+    explored = set()
+    explored.add(start)
+    queue = deque()
+    queue.append(start)
+    while queue:
+        current = queue.popleft()
+        if current in splitter_coords:
+            splits += 1
+
+            # Split
+            for dcol in (-1, 1):
+                adjacent = Point(current.row, current.col+dcol)
+                if adjacent.row < max_row and adjacent.col < max_col and adjacent not in explored:
+                    queue.append(adjacent)
+                    explored.add(adjacent)
+        else:
+            # Move down
+            adjacent = Point(current.row+1, current.col)
+            if adjacent.row < max_row and adjacent.col < max_col and adjacent not in explored:
+                queue.append(adjacent)
+                explored.add(adjacent)
+
+    return splits
 
 if __name__ == '__main__':
     print(f'Sample: {solve(get_input('sample.txt'))}')
