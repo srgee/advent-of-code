853cea976e9974998f1b9944a3ee4b4c9451ccbb
Jorge L. Arienza <sr_gee@yahoo.com>
2025-12-24 12:56:40 +0100

    feat: add working solution for part1 of day 8

--- 2025/day08/part1.py
+++ 2025/day08/part1.py
@@ -1,5 +1,5 @@
-
 from pathlib import Path
+
 
 def get_input(filename: str) -> str:
     '''Read input file and returns a list of text clean lines (removes end of line characters).'''
@@ -14,10 +14,65 @@ def get_input(filename: str) -> str:
 
 
 def solve(data):
-    # TODO: implment solution
-    pass
+    # Parse input data into a list of 3D-coordinates
+    boxes = []
+    for box in data:
+        x, y, z = map(int, box.split(','))
+        boxes.append((x, y, z))   
+    #print(len(boxes))
     
+    # Calculate distance between all possible pairs
+    pairs = []
+    for i, p in enumerate(boxes):
+        for j in range(i+1, len(boxes)):
+            # Calculate squared distance for the sake of performance
+            q = boxes[j]
+            dist = (p[0] - q[0]) ** 2 + (p[1] - q[1]) ** 2 + (p[2] - q[2]) ** 2
+            pairs.append((dist, i, j))  # Store distance and box indices
+    #print(len(pairs))
+
+    # Order pairs by distance in ascending order
+    pairs.sort(key=lambda x: x[0])
+
+    # Disjoint sets (union-find) are the optimal approach for clustering problems.
+    # Use 2 lists: parents (store the parent of each group) and size (store the number of boxes per group)
+    # The structure is initialized so that each box is its own parent. To connect boxes you make one the parent and the other the subordinate
+    # by adding up its sizes. It is more efficient than using graphs and DFS to find the size for each group. With Union-Find you always know the size of the group
+    parents = list(range(len(boxes)))
+    sizes = [1] * len(boxes)
+    
+    def find(i: int) -> int:
+        '''Find the parent.'''
+        if parents[i] != i:
+            parents[i] = find(parents[i])
+        return parents[i]
+
+
+    def union(i: int, j: int) -> bool:
+        '''Merge two groups by adding the smallest group to the bigger group.'''
+        root_i = find(i)
+        root_j = find(j)
+
+        if root_i != root_j:
+            if sizes[root_i] < sizes[root_j]:
+                parents[root_i] = root_j
+                sizes[root_j] += sizes[root_i]
+            else:
+                parents[root_j] = root_i
+                sizes[root_i] += sizes[root_j]
+            return True  # Merged groups successfully
+        return False  # Groups were already merged
+
+    for _, u, v in pairs[:1000]:
+        union(u, v)
+    
+    # Collect & sort circuit sizes. We are only interested in parent-boxes 
+    circuit_sizes = [sizes[i] for i in range(len(boxes)) if parents[i] == i]
+    circuit_sizes.sort(reverse=True)
+
+    return circuit_sizes[0] * circuit_sizes[1] * circuit_sizes[2]
+ 
 
 if __name__ == '__main__':
-    print(f'Sample: {solve(get_input('sample.txt'))}')
+    #print(f'Sample: {solve(get_input('sample.txt'))}')
     print(f'Solution: {solve(get_input('input.txt'))}')
