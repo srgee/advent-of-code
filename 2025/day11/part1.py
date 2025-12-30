1adb0af57c9373ebfcd844e0c2cae33840bc477d
Jorge L. Arienza <sr_gee@yahoo.com>
2025-12-30 13:54:03 +0100

    feat: add working solution for part1 (day 11)

--- 2025/day11/part1.py
+++ 2025/day11/part1.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 def get_input(filename: str) -> str:
@@ -13,11 +12,33 @@ def get_input(filename: str) -> str:
     return input_lines
 
 
-def solve(data):
-    # TODO: implment solution
-    pass
-    
+def solve(data: list[str]) -> int:
+    # Parse input data into a graph: devices are nodes and edges are connections.
+    adjacents = dict()
+    for line in data:
+        device, connections = line.split(':')
+        adjacents[device] = [c for c in connections.split(' ') if len(c) > 1]
+    #print(adjacents)
+
+    # Implement DFS to explore all paths
+    paths = 0
+    devices = ['you']
+    #visited = set()
+    while devices:
+        device = devices.pop()      # Remove last item (LIFO)
+        #if device in visited:
+        #    continue
+
+       #visited.add(device)
+        if adjacents[device] == ['out']:
+            paths += 1
+        else:
+            # Add adjacent devices to explore further. Keep same order as in the adjacents list
+            devices.extend(reversed(adjacents[device]))
+
+    return paths
+
 
 if __name__ == '__main__':
-    print(f'Sample: {solve(get_input('sample.txt'))}')
+    #print(f'Sample: {solve(get_input('sample.txt'))}')
     print(f'Solution: {solve(get_input('input.txt'))}')
